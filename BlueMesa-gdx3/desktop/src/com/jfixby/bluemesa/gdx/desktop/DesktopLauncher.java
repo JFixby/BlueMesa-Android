7aeaeaf79c15f76d459a569fbcb22d8fafd08bde
J Fixby <github@jfixby.com>
2017-05-23 22:05:36 +0200

    +

---
+++ BlueMesa-gdx3/desktop/src/com/jfixby/bluemesa/gdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jfixby.bluemesa.gdx.desktop;
+
+import java.io.IOException;
+
+public class DesktopLauncher {
+    public static void main(final String[] arg) throws IOException {
+        L.d("");
+        com.jfixby.bluemesa.desktop.DesktopLauncher.main(arg);
+
+
+    }
+}
